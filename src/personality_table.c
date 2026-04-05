11dff968e932e7ed1bf24305b793ca75135a553a
Gordon McLellan <gordonthree@gmail.com>
2026-04-04 21:00:05 -0400

    added accessor to find a free personality table slot, init table to 0xFF

--- src/personality_table.c
+++ src/personality_table.c
@@ -23,8 +23,9 @@ uint8_t runtimePersonalityCount = 0;
 int initRuntimePersonalityTable(void)
 {
     uint8_t count = 0;
+
     /** Clear runtimePersonalityTable array */
-    memset(&runtimePersonalityTable, 0, sizeof(personalityDef_t) * MAX_RUNTIME_PERSONALITIES);
+    memset(&runtimePersonalityTable, 0xFF, sizeof(personalityDef_t) * MAX_RUNTIME_PERSONALITIES);
 
     if (g_personalityCount == 0) {
       return -1; /* No personalities defined */
@@ -37,6 +38,24 @@ int initRuntimePersonalityTable(void)
     }
 
     return count;
+}
+
+
+/**
+ * @brief Get the index of a free personality slot in the runtime table.
+ * @details This function iterates over the runtime personality table and returns the index of the first free slot found.
+ * @return The index of a free personality slot if found, -1 if the table is full.
+ */
+int getFreePersonalitySlot(void)
+{
+    for (uint8_t i = 0; i < MAX_RUNTIME_PERSONALITIES; i++) {
+        if (runtimePersonalityTable[i].personalityId == 0xFF) {
+            /* 0xFF indicates an empty slot, matching the 
+            initialization value */
+            return i;
+        }
+    }
+    return -1;
 }
 
 #ifdef __cplusplus
